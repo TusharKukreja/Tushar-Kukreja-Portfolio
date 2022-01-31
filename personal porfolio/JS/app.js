803859fe3dd3cbe1abba17f9ec9393b269194480
Tushar Kukreja <69576076+TusharKukreja@users.noreply.github.com>
2022-01-31 21:07:59 +0530

    Add files via upload

---
+++ personal porfolio/JS/app.js
@@ -0,0 +1,52 @@
+let menu = document.querySelector("#menu-bars");
+let header = document.querySelector("header");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  header.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  header.classList.remove("active");
+};
+
+let cursor1 = document.querySelector(".cursor-1");
+let cursor2 = document.querySelector(".cursor-2");
+
+window.onmousemove = (e) => {
+  cursor1.style.top = e.pageY + "px";
+  cursor1.style.left = e.pageX + "px";
+  cursor2.style.top = e.pageY + "px";
+  cursor2.style.left = e.pageX + "px";
+};
+
+document.querySelectorAll("a").forEach((links) => {
+  links.onmouseenter = () => {
+    cursor1.classList.add("active");
+    cursor2.classList.add("active");
+  };
+
+  links.onmouseleave = () => {
+    cursor1.classList.remove("active");
+    cursor2.classList.remove("active");
+  };
+});
+
+function sendEmail(){
+    Email.send({
+
+        Host : "smtp.google.com",
+        Username : "tusharbirlasoft@gmail.com",
+        Password : "Ju$T4Me!1047",
+        To : "tushar.j.kukreja@gmail.com",
+        From : document.getElementById("email").value,
+        Subject : "New Message From Recruiter",
+        Body : "Name: " + document.getElementById("name").value
+                + "<br> Email: " + document.getElementById("email").value
+                + "<br> Phone Number: " + document.getElementById("number").value
+                + "<br> Message: " + document.getElementById("message").value
+    }).then(
+      message => alert("Thank You for your time!!")
+    );
+}
